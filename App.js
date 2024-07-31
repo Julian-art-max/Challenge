7296ea51bf4e67c9a6fd902441298fbb2880237c
Julian-art-max <juliancacosta3110@gmail.com>
2024-07-31 14:50:21 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+document.getElementById('encryptBtn').addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    let encryptedText = encryptText(inputText);
+    document.getElementById('outputText').innerText = encryptedText;
+});
+
+document.getElementById('decryptBtn').addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    let decryptedText = decryptText(inputText);
+    document.getElementById('outputText').innerText = decryptedText;
+});
+
+document.getElementById('copyBtn').addEventListener('click', function() {
+    let outputText = document.getElementById('outputText').innerText;
+    navigator.clipboard.writeText(outputText).then(function() {
+        alert('Texto copiado al portapapeles');
+    }, function(err) {
+        console.error('Error al copiar el texto: ', err);
+    });
+});
+
+function encryptText(text) {
+    return text.replace(/e/g, 'enter')
+               .replace(/i/g, 'imes')
+               .replace(/a/g, 'ai')
+               .replace(/o/g, 'ober')
+               .replace(/u/g, 'ufat');
+}
+
+function decryptText(text) {
+    return text.replace(/enter/g, 'e')
+               .replace(/imes/g, 'i')
+               .replace(/ai/g, 'a')
+               .replace(/ober/g, 'o')
+               .replace(/ufat/g, 'u');
+}
+
+document.getElementById('inputText').addEventListener('input', function() {
+    let outputText = document.getElementById('outputText');
+    if (this.value === '') {
+        outputText.innerText = 'Ningún mensaje fue encontrado';
+    }
+});
